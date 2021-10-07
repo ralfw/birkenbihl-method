6a0c20f196c790701b150f9746131ade6a162afc
Ralf Westphal <info@ralfw.de>
2021-10-07 18:07:05 +0300

    project for deepl translation

---
+++ src/publish/DeepLTranslation/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace DeepLTranslation
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+
+
+    public class DeepLTranslator
+    {
+        public string Translate(string original)
+        {
+            return "translation";
+        }
+    }
+}
