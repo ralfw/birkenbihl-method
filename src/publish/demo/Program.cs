0314bf6706130ca174c4589408b238fbcc5aeefd
Ralf Westphal <info@ralfw.de>
2021-10-09 20:41:32 +0300

    demo erzeugt ein html mit original und übersetzung

--- src/publish/demo/Program.cs
+++ src/publish/demo/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using DeepLTranslation;
+using Parse;
+using PlayhtTranscription;
 
 namespace demo
 {
@@ -7,8 +13,56 @@ namespace demo
         static void Main(string[] args)
         {
             // Приказки за Дядо Коледа.md
+            var birkenbihl = new BirkenbihlMethod(new DeepLTranslator(), null);
+            birkenbihl.Compile("test.md", "./test");
+        }
+    }
+
+
+    public class BirkenbihlMethod
+    {
+        private readonly DeepLTranslator _translator;
+
+        public BirkenbihlMethod(DeepLTranslator translator, PlayhtTranscriber transcriber) {
+            _translator = translator;
+        }
+
+
+        public void Compile(string mdSourceFilepath, string targetFolderpath)
+        {
+            // publication could be done on vercel!!!
+            // create folder with...
+            //      text.html
+            //      *.mp3
+            if (Directory.Exists(targetFolderpath)) Directory.Delete(targetFolderpath, true);
+            Directory.CreateDirectory(targetFolderpath);
             
-            Console.WriteLine("Hello World!");
+            var text = Parser.Parse(File.ReadAllText(mdSourceFilepath));
+
+            Console.WriteLine("Translating parapgraphs...");
+            var paragraphTranslations = new List<string>();
+            foreach (var paragraph in text.Paragraphs) {
+                Console.WriteLine($"    {paragraph.Text.Substring(0, Math.Min(10, paragraph.Text.Length))}...");
+                var translation = _translator.Translate(paragraph.Text);
+                paragraphTranslations.Add(translation);
+            }
+            
+
+            var html = new StringBuilder();
+            html.AppendLine("<html><body>");
+            for(var i=0; i<text.Paragraphs.Length; i++) {
+                var paragraph = text.Paragraphs[i];
+                if (paragraph.HeadlineLevel > 0) {
+                    html.AppendLine($"<h{paragraph.HeadlineLevel}>{paragraph.Text}</h{paragraph.HeadlineLevel}>");
+                }
+                else {
+                    html.AppendLine($"<p>{paragraph.Text}</p>");
+                }
+                // color names: https://www.w3schools.com/tags/ref_colornames.asp
+                html.AppendLine($"<p style=\"color:gray;\">{paragraphTranslations[i]}</p>");
+            }
+            html.AppendLine("</body></html>");
+            File.WriteAllText(Path.Combine(targetFolderpath, "text.html"), html.ToString());
         }
     }
 }
--- src/publish/demo/readme.md
+++ src/publish/demo/readme.md
@@ -7,15 +7,15 @@
 ## 2. Konvertierung
 1. Absätze übersetzen
 2. Index übersetzen
-3. Absätze transcribieren
+3. Absätze transcribieren // Die Downloads könnten nach dem Hash des Absatzes benannt werden
 
 ## 3. Generieren
 Jetzt den interlinearen Text als HTML-Datei generieren.
 
-- Original Absatz mit Link zur Transcription (.mp3)
+- Original Absatz mit Link zur Transcription (.mp3) // Überschriftenlevel berücksichtigen
 - Übersetzter Absatz
   
 - Interlinearer Absatz: Zeilen à 80 Zeichen
   - Originale Zeile Chunk für Chunk
-  - Übersetzte Zeile Wort für Wort
+  - Übersetzte Zeile Wort für Wort // leere Worte bei nicht-leeren Chunks berücksichtigen
 
